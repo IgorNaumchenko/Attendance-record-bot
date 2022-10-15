9ea6f503c1aa29acb9a9095c1e08a6d02b2c58ea
IgorNaumchenko <nikolay-naumchenko@mail.ru>
2022-10-15 12:37:37 +0300

    Updating core and tgfront

--- core.py
+++ core.py
@@ -8,13 +8,18 @@ class Db_Driver_Sqlite3:
         self.cursor = self.connection.cursor()
 
     def create_table(self, table_name):
-        create_table = f'CREATE TABLE IF NOT EXISTS {table_name}(userid INT PRIMARY KEY, name TEXT, miss INT);'
+        create_table = f'CREATE TABLE IF NOT EXISTS {table_name}(userid AUTO_INCREMENT, name TEXT, miss INT);'
         self.connection.cursor().execute(create_table)
         self.connection.commit()
 
-    def add_miss(self, table_name, ins_tuple):
-        self.cursor.execute(f'INSERT INTO {table_name} VALUES (?, ?, ?);', ins_tuple)
-        self.connection.commit()
+    def add_miss(self, table_name, insert):
+        if len(insert) == 1:
+            f_text = tuple([i for j in insert for i in j])
+            self.cursor.execute(f'INSERT INTO {table_name} VALUES (?, ?, ?);', f_text)
+            self.connection.commit()
+        if len(insert) > 1:
+            self.cursor.executemany(f'INSERT INTO {table_name} VALUES (?, ?, ?);', insert)
+            self.connection.commit()
 
     def show_misses(self, table_name):
         self.cursor.execute(f'SELECT * FROM {table_name};')
--- telegramm_front.py
+++ telegramm_front.py
@@ -13,8 +13,8 @@ async def start(message: types.Message):
     keyboard = InlineKeyboardMarkup()
     login = InlineKeyboardButton('Вход', callback_data='login')
     all_misses = InlineKeyboardButton('Все пропуски', callback_data='all_misses')
-    add_new_miss = InlineKeyboardButton('Добавить пропуски', callback_data='add_new_miss')
-    keyboard.add(login, all_misses, add_new_miss)
+    show_miss_id = InlineKeyboardButton('Показать ID', callback_data='show_id')
+    keyboard.add(login, all_misses, show_miss_id)
 
     await bot.send_message(message.chat.id, 'Приветик\nВыбери функцию\nЕсли Ты не зарегистрирован, зарегайся',
                            reply_markup=keyboard)
@@ -28,7 +28,6 @@ async def login(call: types.callback_query):
         await bot.send_message(call.message.chat.id, f'Вы уже зарегистрированы\nУникальный номер вашей группы "{group_name[0][0]}"')
     except:
         await bot.send_message(call.message.chat.id, 'Усп\nЧто-то пошло не так')
-    # await bot.send_message(call.message.chat.id, f'{userid}')
 
 
 @dp.callback_query_handler(lambda a: a.data == 'all_misses')
@@ -37,6 +36,22 @@ async def all_group_misses(call: types.callback_query):
     miss = Db_Driver_Sqlite3().show_misses(group_name[0][0])
     new = ''.join([f'{i[0]}.{i[1]} - {i[2]}\n' for i in miss])
     await bot.send_message(call.message.chat.id, f'{new}')
+
+
+@dp.callback_query_handler(lambda a: a.data == 'show_id')
+async def show_id(call: types.callback_query):
+    group_name = Db_Driver_Sqlite3().check_login(call.message.chat.id)
+    miss = Db_Driver_Sqlite3().show_misses(group_name[0][0])
+    send = ''.join([f'{i[0]} - {i[1]}\n' for i in miss])
+    await bot.send_message(call.message.chat.id, send)
+
+
+@dp.message_handler()
+async def add_miss(message: types.Message):
+    get_text = [tuple(i.split('-')) for i in message.text.split('\n')]
+    group = Db_Driver_Sqlite3().check_login(message.chat.id)[0][0]
+    for i in get_text:
+        Db_Driver_Sqlite3().update_miss(table_name=group, userid=i[0], adding_miss=i[2])
 
 
 executor.start_polling(dp)
